87258ded9ca6c086424b6eeb7c4bb16ab78a1dff
MeiTzy222 <meitzy222@gmail.com>
2018-10-18 08:37:07 +0800

    my_starwars

--- star_wars_magic_methods/people.py
+++ star_wars_magic_methods/people.py
@@ -1,3 +1,73 @@
 class People(object):
-    def __init__(self, name, dark_side=False):
-        pass
+    def __init__(self, name, dark_side=False, light_side=True):
+        self.name = name
+        self.dark_side = dark_side
+        self.light_side = light_side
+        
+        if self.dark_side == True:
+            self.light_side = False
+
+    def __str__(self):
+        return str(self.name)
+
+    def __call__(self):
+        return("Help me {}, you\'re my only hope.".format(self.name))
+
+    def __truediv__(self, other):
+        if isinstance(other, People):
+            return("{} swings a lightsaber at {}.".format(self, other))
+        else:
+            raise TypeError()
+
+    def __mul__(self, other):
+        if isinstance(other, People):
+            return("{} throws a thermal detonator at {}!".format(self, other))
+        else:
+            raise TypeError()
+
+    def __rshift__(self, other):
+        if isinstance(other, People):
+            return("{} uses the force to push {} away from them.".format(self, other))
+        else:
+            raise TypeError()
+
+    def __lshift__(self, other):
+        if isinstance(other, People):
+            return("{} uses the force to pull {} towards them.".format(self, other))
+        else:
+            raise TypeError()
+
+    def __neg__(self):
+        self.dark_side = True
+        self.light_side = False
+    
+    def __pos__(self):
+        if self.dark_side == True:
+            self.dark_side = False
+            self.light_side = True
+
+    def __invert__(self):
+        if self.light_side == True:
+            self.light_side = False
+        elif self.light_side == False:
+            self.light_side = True
+
+        if self.dark_side == True:
+            self.dark_side = False
+        elif self.dark_side == False:
+            self.dark_side = True
+
+    def __xor__(self, other):
+        return("{} force chokes {}.".format(self, other))
+
+    def __eq__(self, other):
+        if self.name == "Han Solo":
+            return("{} shoots {}.".format(self, other))
+        else:
+            return("Han Solo shoots {}. BECAUSE HAN SHOOTS FIRST.".format(self))
+
+
+
+
+
+
--- star_wars_magic_methods/saber_crystal.py
+++ star_wars_magic_methods/saber_crystal.py
@@ -1,3 +1,60 @@
 class SaberCrystal(object):
     def __init__(self, color=(255,0,0)):
-        pass
+        self.red = color[0]
+        self.green = color[1]
+        self.blue = color[2]
+        self.color = (self.red, self.green, self.blue)
+    
+    def __eq__(self, other):
+        return self.color == other.color
+ 
+    def __add__(self, other):
+        # Solution #1: Check data type of 'other'
+        # if type(other) is tuple:
+        #     result = tuple(sum(x) for x in zip(self.color, other))
+        # else:
+        #     result = tuple(sum(x) for x in zip(self.color, other.color))
+        # return SaberCrystal(color=result)
+
+        # Solution #2: Check whether 'other' is an instance of SaberCrystal
+        if isinstance(other, SaberCrystal):
+            my_zip = zip(self.color, other.color)
+        else:
+            my_zip = zip(self.color, other)
+        
+        result = [sum(x) for x in my_zip]
+
+        # for loop approach
+        # new_result = []
+        # for i in result:
+        #     if i > 255:
+        #         new_result.append(255)
+        #     else:
+        #         new_result.append(i)
+
+        # List comprehension approach: expression -> if-else condition -> iterable 
+        new_result = [255 if i > 255 else i for i in result]
+        return SaberCrystal(color = tuple(new_result))
+
+    def __sub__(self, other):
+        if isinstance(other, SaberCrystal):
+            my_zip = zip(self.color, other.color)
+        else:
+            my_zip = zip(self.color, other)
+       
+        result = [abs(x-y) for x, y in my_zip]
+        new_result = [255 if i > 255 else i for i in result]
+        return SaberCrystal(color = tuple(new_result))
+
+    def __contains__(self, other):       
+        if isinstance(other, SaberCrystal):
+            my_zip = zip(self.color, other.color)
+        else:
+            my_zip = zip(self.color, other)
+        
+        checklist = [True for x, y in my_zip if x == y and x != 0]
+        if True in checklist:
+            return True
+        # else:
+        #     return False
+        
